ff5555db3cc8554e7382dc4a2852a28705ae6d92
galleg0s <krivomariia@gmail.com>
2023-04-24 13:56:27 -0400

    Replace mongo url with env variable

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL = "mongodb+srv://nasa-api:TF4934oo22964@cluster0.hxzpmk2.mongodb.net/nasa?retryWrites=true&w=majority";
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.on('open', () => {
     console.log('MongoDB connection ready');
