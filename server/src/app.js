9cb672abab64fa65555fc39186be30e19833a651
galleg0s <krivomariia@gmail.com>
2023-04-28 10:29:25 -0400

    Add mongodb mock

--- server/src/app.js
+++ server/src/app.js
@@ -13,10 +13,10 @@ require('dotenv').config();
 const app = express(); 
 
 const AUTH_CONFIG = {
-    CLIENT_ID: process.env.CLIENT_ID,
-    CLIENT_SECRET: process.env.CLIENT_SECRET,
-    COOKIE_KEY_1: process.env.COOKIE_KEY_1,
-    COOKIE_KEY_2: process.env.COOKIE_KEY_2,
+    CLIENT_ID: process.env.CLIENT_ID || '',
+    CLIENT_SECRET: process.env.CLIENT_SECRET || '',
+    COOKIE_KEY_1: process.env.COOKIE_KEY_1 || '',
+    COOKIE_KEY_2: process.env.COOKIE_KEY_2 || '',
 }
 
 const AUTH_OPTIONS = {
